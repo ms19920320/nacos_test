2f0aeb6322a78762387ff7fcf1b39175bd4884c9
mengshuai <mengshuai@citycloud.com.cn>
2022-04-29 08:27:06 +0800

    nacos发现配置

--- score/src/main/java/com/citycloud/nacostest/score/controller/ScoreController.java
+++ score/src/main/java/com/citycloud/nacostest/score/controller/ScoreController.java
@@ -1,10 +1,12 @@
 package com.citycloud.nacostest.score.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author 孟帅
@@ -17,6 +19,9 @@ public class ScoreController {
     @Value("${name}")
     private String name;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/test")
     public String test() {
         return "score test";
@@ -25,5 +30,11 @@ public class ScoreController {
     @GetMapping("/getName")
     public String getName() {
         return name;
+    }
+
+    @GetMapping("/orderTest")
+    public String orderTest() {
+        String forObject = restTemplate.getForObject("", String.class);
+        return forObject;
     }
 }
