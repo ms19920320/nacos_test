1e1484afea5ec3de9c7b1e54df089eb9cf0efde0
mengshuai <mengshuai@citycloud.com.cn>
2022-12-30 14:21:56 +0800

    sentinel测试代码实现

--- score/src/main/java/com/citycloud/nacostest/score/controller/ScoreTestController.java
+++ score/src/main/java/com/citycloud/nacostest/score/controller/ScoreTestController.java
@@ -1,6 +1,12 @@
 package com.citycloud.nacostest.score.controller;
 
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.citycloud.nacostest.common.exception.ResValue;
 import com.citycloud.nacostest.score.mapper.TestUserMapper;
 import com.citycloud.nacostest.score.feign.IOrderService;
@@ -9,6 +15,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -32,7 +42,28 @@ public class ScoreTestController {
 
     @GetMapping(value = "/aa")
     public ResValue aa() {
-        return orderService.aa();
+        ResValue res = ResValue.failed();
+        // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+        try (Entry entry = SphU.entry("HelloWorld")) {
+            System.out.println("normal!");
+            res = orderService.aa();
+        } catch (BlockException ex) {
+            // 处理被流控的逻辑
+            System.out.println("blocked!");
+        }
+        return res;
+    }
+
+    @PostConstruct
+    private void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(10);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 
 }
